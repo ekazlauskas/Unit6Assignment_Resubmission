8d2a1e927d0c011695bd96ed1935c2d2bc129222
patriotfever4@gmail.com <Ek@Z0463>
2019-03-02 12:12:10 -0600

    Pushed Partial Code

---
+++ app/src/main/java/css/unit7assignment/FlooringResults.java
@@ -0,0 +1,16 @@
+package css.unit7assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FlooringResults extends AppCompatActivity {
+
+    TextView FlooringRequested;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flooring_results);
+    }
+}
